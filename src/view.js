5c7e331e69d9aefaba1d52e66aff8d5bb8126551
Evgeniy Larin <nothinghill15@gmail.com>
2023-06-23 15:35:26 -0400

    css

--- src/view.js
+++ src/view.js
@@ -122,7 +122,7 @@ const renderFeeds = (state, elements, i18n) => {
 
       const postLink = document.createElement('a');
       postLink.href = post.link;
-      postLink.classList.add(state.openedPost.has(post.id) ? 'fw-normal link-secondary' : 'fw-bold');
+      postLink.classList.add(state.openedPost.has(post.id) ? ('fw-normal', 'link-secondary') : 'fw-bold');
       postLink.dataset.id = post.id;
       postLink.target = '_blank';
       postLink.rel = 'noopener noreferrer';
